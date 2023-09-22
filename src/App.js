e4bef863dfde3ea874e7a2d8eed83ad69fc74b9b
VIMLESH KUMAR <vimlesh1975@hotmail.com>
2023-09-22 18:10:45 +0530

    r3f

--- src/App.js
+++ src/App.js
@@ -1,25 +1,78 @@
-import logo from './logo.svg';
-import './App.css';
+// import * as THREE from 'three';
+import { useEffect } from 'react';
+import { Canvas } from '@react-three/fiber';
+import { getProject } from '@theatre/core';
+import studio from '@theatre/studio';
+import extension from '@theatre/r3f/dist/extension';
+import { editable as e, SheetProvider } from '@theatre/r3f';
 
-function App() {
+import { PerspectiveCamera } from '@theatre/r3f';
+
+import demoProjectState from './state.json';
+
+studio.initialize();
+studio.extend(extension);
+
+const project = getProject('Demo Project', { state: demoProjectState });
+
+const demoSheet = project.sheet('Demo Sheet');
+
+// const demoSheet = getProject('Demo Project').sheet('Demo Sheet');
+
+const App = () => {
+  useEffect(() => {
+    demoSheet.project.ready.then(() =>
+      demoSheet.sequence.play({ iterationCount: Infinity, range: [0, 1] })
+    );
+  }, []);
+
+  const aa = (i) => {
+    return (
+      <e.mesh theatreKey={'Cube' + i.toString()}>
+        <boxGeometry args={[1, 1, 1]} />
+        <meshStandardMaterial color="orange" />
+      </e.mesh>
+    );
+  };
+
+  useEffect(() => {
+    for (let i = 1; i <= 5; i++) {
+      console.log(i);
+    }
+
+    return () => {};
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      style={{
+        height: 1080,
+        backgroundColor: 'grey',
+        border: '1px solid red',
+      }}
+    >
+      <Canvas>
+        <SheetProvider sheet={demoSheet}>
+          <PerspectiveCamera
+            theatreKey="Camera"
+            makeDefault
+            position={[5, 5, -5]}
+            fov={75}
+          />
+          <ambientLight />
+          <e.pointLight theatreKey="Light" position={[10, 10, 10]} />
+          {['a', 'b', 'c', 'd'].map((val, i) => {
+            return (
+              <e.mesh theatreKey={'Cube' + i.toString()}>
+                <boxGeometry args={[1, 1, 1]} />
+                <meshStandardMaterial color="orange" />
+              </e.mesh>
+            );
+          })}
+        </SheetProvider>
+      </Canvas>
     </div>
   );
-}
+};
 
 export default App;
